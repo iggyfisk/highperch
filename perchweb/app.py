d928aa5001ea910634c60fc8518bf9c3b87985e7
Sverre <sverre.westerlund@gmail.com>
2019-11-22 01:31:23 +0100

    Query string the changing CSS and JS when changed

--- perchweb/app.py
+++ perchweb/app.py
@@ -3,9 +3,11 @@ The flask application package.
 """
 
 from os import environ, path
+from glob import glob
+from hashlib import md5
+import base64
 from flask import Flask
 from views import routes
-
 from replaydb import close_connection as close_replaydb
 
 app = Flask(__name__)
@@ -23,6 +25,15 @@ if environ.get('HIGHPERCH_ENVIRONMENT') == "production":
     app.secret_key = environ.get('HIGHPERCH_FLASK_KEY')
 else:
     app.secret_key = 'debug'
+
+mutable_static = glob(path.join(app.root_path, 'static/style', '*.css')) + \
+    glob(path.join(app.root_path, 'static/script', '*.js'))
+
+# Cache bust CSS and JS which may have changed, quick n dirty
+hash_obj = md5(open(mutable_static[0], 'rb').read())
+for static_file in mutable_static[1:]:
+    hash_obj.update(open(static_file, 'rb').read())
+app.config['STATIC_HASH'] = base64.urlsafe_b64encode(hash_obj.digest()).decode('ascii')[:-2]
 
 if __name__ == "__main__":
     app.run()
