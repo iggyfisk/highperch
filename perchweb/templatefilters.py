74ec468b8343b73c22d9fc1ee7891efe02215dd2
Sverre <sverre.westerlund@gmail.com>
2019-11-24 02:20:52 +0100

    Convert map names to thumbnails, closes #6

--- perchweb/templatefilters.py
+++ perchweb/templatefilters.py
@@ -1,5 +1,7 @@
 """ Jinja2 utility filters """
-
+from os.path import isfile, join
+from re import compile as re_compile
+from filepaths import get_path
 
 def gametime(milliseconds):
     """ Convert milliseconds to readable timestamp, like 34:04 """
@@ -30,9 +32,29 @@ race_titles = {
     'N': 'Night Elf',
     'U': 'Noob Alert'
 }
+
+
 def race_title(race):
     """ Title text for single letter race ID """
     return race_titles[race]
+
+
+thumbnails = {}
+mapsize = re_compile('^\\(\\d\\)$')
+def map_thumbnail(map_name):
+    """ Map image src for a given map name, placeholder thumbnail if a real one can't be found """
+    if map_name in thumbnails:
+        return thumbnails[map_name]
+    file_name = map_name[:-3] if map_name[-3:] == '_LV' else map_name
+    file_name = file_name[3:] if mapsize.match(file_name[:3]) else file_name
+    file_name = file_name.lower()
+    if isfile(get_path(join('static', 'images', 'minimaps', f'{file_name}.jpg'))):
+        thumbnails[map_name] = f'/static/images/minimaps/{file_name}.jpg'
+    else:
+        # Todo: real placeholder
+        thumbnails[map_name] = f'/static/images/minimaps/fakethumbnail.jpg'
+    return thumbnails[map_name]
+
 
 
 def register(jinja_environment):
@@ -41,3 +63,4 @@ def register(jinja_environment):
     jinja_environment.filters['chatmode'] = chatmode
     jinja_environment.filters['raceicon'] = race_icon
     jinja_environment.filters['racetitle'] = race_title
+    jinja_environment.filters['mapthumbnail'] = map_thumbnail
