353c21625d9779cf94f6aec9994988dcba7318a6
Andreas Carlson <andreascarlson@gmail.com>
2019-11-28 01:23:53 +0100

    alignments

--- perchweb/scss/build.py
+++ perchweb/scss/build.py
@@ -1,6 +1,6 @@
 import sass
 
-# builds the perch.css file once. To watch the directory for changes, you can do:
+# builds the perch.css file once. To watch the directory for changes, you can do from the /perchweb directory:
 # npm install -g sass
 # sass --watch scss:static/style
 
